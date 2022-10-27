25b5b603a0cf1f19fa7c4de8570f04240973ecc6
cklewar@t-online.de <cklewar@t-online.de>
2022-10-28 01:11:00 +0200

    init

---
+++ versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.3.0"
+  cloud {
+    organization = "cklewar"
+    hostname     = "app.terraform.io"
+
+    workspaces {
+      name = "f5-xc-blindfold-module"
+    }
+  }
+
+  required_providers {
+    local = ">= 2.2.3"
+    null  = ">= 3.1.1"
+    http  = ">= 3.1.0"
+  }
+}
